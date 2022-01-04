daf163f2800584a245121a3a9fa9572a9137afb6
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-04 18:17:35 -0500

    created homepage html route

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 const path = require("path");
 
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
 module.exports = router;
