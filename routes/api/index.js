384f26115007a9251512accc8c250ac63175055f
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-04 18:56:44 -0500

    fixed routing for api

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,12 +2,23 @@ const router = require("express").Router();
 const workout = require("../../models/Workout.js");
 
 //get all workouts
-router.get("/api/workouts", async (req, res) => {
+router.get("/workouts", async (req, res) => {
   workout
     .find()
     .then((data) => {
       console.log(data);
       res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/workouts/range", (req, res) => {
+  workout
+    .find({})
+    .then((workout) => {
+      res.json(workout);
     })
     .catch((err) => {
       res.json(err);
