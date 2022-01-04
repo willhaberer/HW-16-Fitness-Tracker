f35fff7afbfaab6ff14bab50c485815f154e1be6
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-03 22:27:54 -0500

    added html folder within routes and required it

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require("express").Router();
 const apiRoutes = require("./api");
+const htmlRoutes = require("./html");
 
 routes.use("/api", apiRoutes);
+routes.use("./", htmlRoutes);
 
 module.exports = routes;
