5de8135687c2d5287cf0765f1797f76eb26372b2
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-04 18:18:41 -0500

    creaeted stats html route

--- routes/html/index.js
+++ routes/html/index.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/stats.html"));
+});
+
 module.exports = router;
