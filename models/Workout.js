d2542097ce9761afbc457c55fa1be3486439ccdb
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-04 20:01:19 -0500

    changed workout model to reference exercise

--- models/Workout.js
+++ models/Workout.js
@@ -3,33 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  exercises: [
-    {
-      type: {
-        type: String,
-      },
-      name: {
-        type: String,
-      },
-      distance: {
-        type: Number,
-      },
-      duration: {
-        type: Number,
-      },
-      weight: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-    },
-  ],
-  //need something for day
   day: {
     type: Date,
     default: () => new Date(),
   },
+  exercises: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Exercise",
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 module.exports = {
   Workout: require("./Workout.js"),
+  Exercise: require("./Exercise.js"),
 };
